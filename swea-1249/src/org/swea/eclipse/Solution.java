2f781cceaba4df1b893885b1a89d7038837d3ae5
Juyoung Kim <kimmykim.jy@gmail.com>
2022-07-09 22:12:13 +0900

    new project

---
+++ swea-1249/src/org/swea/eclipse/Solution.java
@@ -0,0 +1,29 @@
+package org.swea.eclipse;
+
+import java.util.*;
+
+public class Solution {
+
+	public static final int[] dy= {0,0,1,-1};
+	public static final int[] dx= {1,-1,0,0};
+	
+	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Scanner sc=new Scanner (System.in);
+		int T=sc.nextInt();
+		
+		for (int tc=1; tc<=T; tc++) {
+			int N=sc.nextInt();
+			
+			for (int i=0; i<N; i++) {
+				string num=sc.nextLine();
+			}
+		}
+		
+
+	}
+
+}
